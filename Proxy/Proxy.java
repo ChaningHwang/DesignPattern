969a194df7a6ef3c66124c714c85fe3470f6e2d1
ChaningHwang <57698098+ChaningHwang@users.noreply.github.com>
2020-06-06 15:32:18 -0500

    Proxy template

---
+++ Proxy/Proxy.java
@@ -0,0 +1,12 @@
+package Proxy;
+
+public class Proxy extends Subject {
+    RealSubject realSubject;
+    @Override
+    public void Request() {
+        if(realSubject == null) {
+            realSubject = new RealSubject();
+        }
+        realSubject.Request();
+    }
+}
---
+++ Proxy/RealSubject.java
@@ -0,0 +1,8 @@
+package Proxy;
+
+public class RealSubject extends Subject {
+    @Override
+    public void Request() {
+        System.out.println("Real request");
+    }
+}
